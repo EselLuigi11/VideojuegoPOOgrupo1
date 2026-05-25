8da463988195d121d6066880255517fa6b050b6e
Ignacioho07 <137006133+Ignacioho07@users.noreply.github.com>
2026-05-25 12:35:06 -0300

    Update Batalla.java

--- VideoJuegoPOO/src/modelo/Batalla.java
+++ VideoJuegoPOO/src/modelo/Batalla.java
@@ -1,5 +1,50 @@
 package modelo;
 
-public class Batalla {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Batalla {
+	private List<Heroe> heroes;
+	private List<Enemigo> enemigos;
+	private EstadoBatalla estadoActual;
+	
+	// Constructor
+	public Batalla (List<Heroe> heroes, List<Enemigo> enemigos) {
+		this.heroes = heroes;
+		this.enemigos = enemigos;
+		estadoActual = EstadoBatalla.EN_CURSO;
+	}
+	
+	public EstadoBatalla evaluarEstado() {
+	    
+	    boolean todosHeroesMuertos = true; 
+	    
+	    for (Heroe h : heroes) {
+	        if (h.estaVivo()) {
+	            todosHeroesMuertos = false;
+	        }
+	    }
+
+	    boolean todosEnemigosMuertos = true;
+	    
+	    for (Enemigo e : enemigos) {
+	    	if (e.estaVivo()) {
+	    		todosEnemigosMuertos = false;
+	    	}
+	    }
+
+	    
+	    if (todosHeroesMuertos) {
+	    	return EstadoBatalla.DERROTA;
+	    }
+	    
+	    else if (todosEnemigosMuertos) {
+	    	return EstadoBatalla.VICTORIA;
+	    }
+	    
+	    else {
+	    	return EstadoBatalla.EN_CURSO;
+	    }
+
+	}
 }
