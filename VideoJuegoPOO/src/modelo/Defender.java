fa7c878e46ef89e8e85b7ffeceb6b61bc6f0f5ac
JMsamuraiX4 <juanmartinramosgr@gmail.com>
2026-05-27 00:06:21 -0300

    Update Defender.java

--- VideoJuegoPOO/src/modelo/Defender.java
+++ VideoJuegoPOO/src/modelo/Defender.java
@@ -1,5 +1,13 @@
-package modelo;
+public class Defender implements Accion {
+    private Entidad defensor;
 
-public class Defender extends Accion {
+    public Defender(Entidad defensor) {
+        this.defensor = defensor;
+    }
 
+    @Override
+    public void ejecutar() {
+        System.out.println("🛡️ " + defensor.getNombre() + " adopta una postura defensiva.");
+        // Lógica para subir defensa temporal (dependerá de cómo Ignacio arme la Entidad)
+    }
 }
