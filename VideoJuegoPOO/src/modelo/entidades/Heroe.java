d677d0dd8d99a69d4d31f6e61775b5ae85b63bae
Ignacioho07 <137006133+Ignacioho07@users.noreply.github.com>
2026-06-01 19:21:28 -0300

    Update Heroe.java

--- VideoJuegoPOO/src/modelo/entidades/Heroe.java
+++ VideoJuegoPOO/src/modelo/entidades/Heroe.java
@@ -14,6 +14,7 @@ public class Heroe extends Entidad {
 	private int experiencia;
 	private int nivel;
 	private int energia;
+	private int energiaMax;
 	private int mana;
 	private int manaMax;
 	private int probCrit;
@@ -25,12 +26,13 @@ public class Heroe extends Entidad {
 	
 	
 	public Heroe(String nombre, int vida, int vidaMax, int ataque, int defensa, int velocidad, boolean estaDefendiendo,
-			int experiencia, int nivel, int energia, int mana, int manaMax, int probCrit, int danoCrit,
+			int experiencia, int nivel, int energia, int energiaMax, int mana, int manaMax, int probCrit, int danoCrit,
 			List<Habilidad> habilidades, Arma arma, Armadura armadura) {
 		super(nombre, vida, vidaMax, ataque, defensa, velocidad, estaDefendiendo);
 		this.experiencia = experiencia;
 		this.nivel = nivel;
 		this.energia = energia;
+		this.energiaMax = energiaMax;
 		this.mana = mana;
 		this.manaMax = manaMax;
 		this.probCrit = probCrit;
@@ -58,6 +60,22 @@ public class Heroe extends Entidad {
 		}
 	}
 	
+	public void recuperarMana(int cantidad) {
+		this.mana += cantidad;
+		
+		if (this.mana > this.manaMax) {
+			this.mana = this.manaMax;
+		}
+	}
+	
+	public void recuperarEnergia(int cantidad) {
+		this.energia += cantidad;
+		
+		if (this.energia > this.energiaMax) {
+			this.energia = this.energiaMax;
+		}
+	}
+	
 	public void ganarExperiencia(int exp) {
 		this.experiencia += exp;
 		if (this.experiencia >= nivel * 105) {
@@ -74,6 +92,8 @@ public class Heroe extends Entidad {
 		 this.setDefensa(this.getDefensa() + 5);
 		 this.setManaMax(this.getManaMax() + 10);
 		 this.setMana(this.getManaMax());
+		 this.energiaMax += 10;
+		 this.energia = this.energiaMax;
 	}
 	
 	
@@ -162,6 +182,4 @@ public class Heroe extends Entidad {
 	public void setArmadura(Armadura armadura) {
 		this.armadura = armadura;
 	}
-
-	
 }
