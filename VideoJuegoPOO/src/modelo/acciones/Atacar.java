4cfe0ca2cb19d98c2d1ca7424de85488e9041e60
JMsamuraiX4 <juanmartinramosgr@gmail.com>
2026-05-31 21:33:51 -0300

    Add Atacar class for attack actions

---
+++ VideoJuegoPOO/src/modelo/acciones/Atacar.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Atacar implements Accion {
+    private Entidad atacante;
+    private Entidad objetivo;
+    private int dano;
+
+    public Atacar(Entidad atacante, Entidad objetivo, int dano) {
+        this.atacante = atacante;
+        this.objetivo = objetivo;
+        this.dano = dano;
+    }
+
+    @Override
+    public void ejecutar() {
+        System.out.println(atacante.getNombre() + " ataca a " + objetivo.getNombre() + " por " + dano + " de daño.");
+        objetivo.recibirDano(dano); 
+    }
+}
