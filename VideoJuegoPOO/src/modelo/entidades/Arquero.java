bcb6b6f64f6c18b69a3390e0704ed42bceebf307
Ignacioho07 <137006133+Ignacioho07@users.noreply.github.com>
2026-06-20 19:23:19 -0300

    Update Arquero.java

--- VideoJuegoPOO/src/modelo/entidades/Arquero.java
+++ VideoJuegoPOO/src/modelo/entidades/Arquero.java
@@ -1,37 +1,42 @@
 package modelo.entidades;
 
+import java.util.List;
+
 import modelo.Arma;
 import modelo.Armadura;
 import modelo.habilidades.HabEspArquero;
 
 public class Arquero extends Heroe implements java.io.Serializable {
-    private static final long serialVersionUID = 1L;
-    private HabEspArquero habilidadEspecial;
+	private static final long serialVersionUID = 1L;
+	private HabEspArquero habilidadEspecial;
 
-    public Arquero(String nombre, Arma arma, Armadura armadura) {
-        super(nombre,
-                95, 95,      // vida, vidaMax
-                23, 10, 18,  // ataque, defensa, velocidad
-                false,
-                0, 1,        // experiencia, nivel
-                70, 70,      // mana, manaMax
-                30, 160,     // probCrit, danoCrit
-                arma, armadura);
-        this.habilidadEspecial = new HabEspArquero();
+	public Arquero(String nombre, Arma arma, Armadura armadura) {
+		super(nombre,
+				95, 95,
+				23, 10, 18,
+				false,
+				0, 1,
+				70, 70,
+				30, 160,
+				arma, armadura);
+		this.habilidadEspecial = new HabEspArquero();
 
-        // (vidaMax, ataque, defensa, velocidad, manaMax, probCrit, danoCrit)
-        tablaDeNiveles.put(2, new StatsNivel(110, 37, 12, 20, 80,  34, 168));
-        tablaDeNiveles.put(3, new StatsNivel(125, 46, 14, 22, 90,  38, 178));
-        tablaDeNiveles.put(4, new StatsNivel(140, 56, 16, 24, 100, 43, 190));
-        tablaDeNiveles.put(5, new StatsNivel(155, 67, 18, 26, 110, 49, 205));
-    }
+		tablaDeNiveles.put(2, new StatsNivel(110, 37, 12, 20, 80, 34, 168));
+		tablaDeNiveles.put(3, new StatsNivel(125, 46, 14, 22, 90, 38, 178));
+		tablaDeNiveles.put(4, new StatsNivel(140, 56, 16, 24, 100, 43, 190));
+		tablaDeNiveles.put(5, new StatsNivel(155, 67, 18, 26, 110, 49, 205));
+	}
 
-    @Override
-    public void subirNivel() {
-        super.subirNivel();
-        System.out.println("  ¡El Arquero " + getNombre() + " subió de nivel!");
-    }
+	@Override
+	public String usarHabilidadEspecial(Enemigo objetivo, List<Enemigo> enemigosVivos, List<Heroe> aliadosVivos) {
+		return habilidadEspecial.ejecutar(this, objetivo);
+	}
+
+	@Override
+	public void subirNivel() {
+		super.subirNivel();
+	}
 
 	public HabEspArquero getHabilidadEspecial() { return habilidadEspecial; }
-	public void setHabilidadEspecial(HabEspArquero habilidadEspecial) { this.habilidadEspecial = habilidadEspecial; }
+	public void setHabilidadEspecial(HabEspArquero h) { this.habilidadEspecial = h; }
 }
